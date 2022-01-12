f08dae89cddb7f4ed8df32ebfe5beb369c4e4f5e
jakedoublev <vanvorhisjake@gmail.com>
2022-01-11 19:51:22 -0800

    basic flight selector component in development, state handling next

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import App from './components/App';
-import About from './components/About';
-import VaccineMap from './components/VaccineMap';
-import CovidMap from './components/CovidMap';
-import Styles from './stylesheets/styles.css';
+import React from "react";
+import { render } from "react-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import App from "./components/App";
+import About from "./components/About";
+import VaccineMap from "./components/VaccineMap";
+import CovidMap from "./components/CovidMap";
+import Styles from "./stylesheets/styles.css";
+import FlightSelector from "./components/FlightSelector";
 
 render(
   <BrowserRouter>
@@ -13,11 +14,17 @@ render(
       <Route path="/" element={<App />}>
         <Route path="" element={<CovidMap />} />
         <Route path="home" element={<CovidMap />} />
-        <Route path="country" element={<VaccineMap />} render={(props) => <VaccineMap {...props}/>}/>
+        <Route
+          path="country"
+          element={<VaccineMap />}
+          render={(props) => <VaccineMap {...props} />}
+        />
         <Route path="about" element={<About />} />
+        {/* temporarily added a selector path to load for implementation of front-end */}
+        <Route path="selector" element={<FlightSelector />} />
       </Route>
     </Routes>
   </BrowserRouter>,
 
-  document.getElementById('root')
+  document.getElementById("root")
 );
