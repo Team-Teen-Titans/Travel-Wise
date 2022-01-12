f3e383597d577991a9abe26df04b8948f7511b5a
jakedoublev <vanvorhisjake@gmail.com>
2022-01-11 20:49:20 -0800

    flight selector work in progress

--- src/components/FlightSelector.jsx
+++ src/components/FlightSelector.jsx
@@ -1,36 +1,79 @@
 import React, { useState, useEffect } from "react";
+import { flightKey, navigate } from "../utils/constants";
+import axios from "axios";
 
 const FlightSelector = () => {
-  const [howManyWays, setHowManyWays] = useState(null);
-  const [tripInfo, setTripInfo] = useState(null);
+  const [tripInfo, setTripInfo] = useState({
+    originAirport: null,
+    destinationAirport: null,
+    departureDate: null,
+    arrivalDate: null,
+    oneWayOrRound: null,
+  });
 
+  useEffect(() => {
+    console.log(tripInfo);
+  });
+
+  const handleSearch = (e) => {
+    console.log(tripInfo);
+    navigate("/flights", { state: { tripInfo } });
+  };
+
+  const getAirportCode = (input) => {
+    axios.request();
+  };
+
+  //need to set all the state with each input type
+  //need to get the first airport code for each city to pass as state to next component
+  //need to render extra set of date for round trip and set that state
   return (
-    <div className=" ">
+    <div className="place-center">
       <h3>Type of trip?</h3>
       <span>
-        <button onClick={() => setHowManyWays(1)}>one way</button>
+        <button
+          onClick={() =>
+            setTripInfo({ ...tripInfo, oneWayOrRound: "onewaytrip" })
+          }
+          className="border 1px rounded"
+          id="one-way"
+        >
+          one way
+        </button>
         {" or "}
-        <button onClick={() => setHowManyWays(2)}>round trip</button>
+        <button
+          onClick={() =>
+            setTripInfo({ ...tripInfo, oneWayOrRound: "roundtrip" })
+          }
+          className="border 1px rounded"
+          id="round-trip"
+        >
+          round trip
+        </button>
       </span>
 
       <br />
       <br />
 
       <span>
-        <p>Departure Date: </p>
+        <label for="departure-date">Departure Date: </label>
         <input type="date"></input>
         {"  "}
-        <p>Return Date: </p>
+        <label for="return-date">Return Date: </label>
         <input type="date"></input>
       </span>
 
       <span>
-        <p>From City: </p>
+        <label for="origin-city">From City: </label>
         <input></input>
         {"  "}
-        <p>Destination: </p>
+        <label for="destination-city">Destination: </label>
         <input></input>
       </span>
+
+      <br />
+      <br />
+      <button onClick={handleSearch}>Search Flights Now</button>
     </div>
   );
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,27 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const Dotenv = require('dotenv-webpack');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
-  entry: path.join(__dirname, 'src/index.js'),
+  entry: path.join(__dirname, "src/index.js"),
   devServer: {
-    static: path.join(__dirname, 'build'),
+    static: path.join(__dirname, "build"),
     compress: true,
     port: 8080,
     proxy: {
-      '/': {
-        target: 'http://localhost:3000/',
+      "/": {
+        target: "http://localhost:3000/",
       },
     },
   },
 
   output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js",
   },
 
-  devtool: 'inline-source-map',
+  devtool: "inline-source-map",
   mode: process.env.NODE_ENV,
 
   module: {
@@ -30,29 +30,29 @@ module.exports = {
         test: /\.jsx?/,
         exclude: /(node_modules)/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
+            presets: ["@babel/preset-env", "@babel/preset-react"],
           },
         },
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
       },
     ],
   },
   resolve: {
-    extensions: ['.js', '.jsx'],
+    extensions: [".js", ".jsx"],
   },
 
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Dev',
-      template: path.join(__dirname, 'index.html'),
+      title: "Dev",
+      template: path.join(__dirname, "index.html"),
     }),
     new MiniCssExtractPlugin({
-      filename: 'styles.css',
+      filename: "styles.css",
     }),
     new Dotenv(),
   ],
