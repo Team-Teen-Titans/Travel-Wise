3cb30eacf05b570f3ad7f311451b3e3c0c0f767e
Kylene Hohman <14060565+velveteen-ant@users.noreply.github.com>
2022-01-15 14:03:55 -0500

    added flight info to card

---
+++ src/components/FlightFeed.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FlightFeed = () => {
+    return (
+        <div>
+        <h3>Available Flights</h3>
+        {/* {flights} */}
+            
+        </div>
+    )
+}
+
+export default FlightFeed;
