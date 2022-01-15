9f1307fd00bd8033c0d7becda740e2c82cfbc9e8
Kylene Hohman <14060565+velveteen-ant@users.noreply.github.com>
2022-01-15 14:38:20 -0500

    added link to homepage in flight feed

--- src/components/FlightFeed.jsx
+++ src/components/FlightFeed.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React from 'react';
+import FlightCard from './FlightCard';
+import { Link } from 'react-router-dom';
 
-const FlightFeed = () => {
-    return (
-        <div>
-        <h3>Available Flights</h3>
-        {/* {flights} */}
-            
-        </div>
-    )
-}
+const FlightFeed = ({ flights }) => {
+  return (
+    <div>
+      <h3>Available Departing Flights</h3>
+      {flights.map((flightInfo, index) => (
+        <FlightCard key={index} flightInfo={flightInfo} />
+      ))}
+
+      {flights.length === 0 && <p> No flights available. Click <Link to={'/home'}>here</Link> to return to the home page and search again.</p>}
+
+    </div>
+  );
+};
 
 export default FlightFeed;
