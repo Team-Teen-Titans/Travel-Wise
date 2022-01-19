3f7168e739a3976930f8e761b4b474498f4f2aee
Kylene Hohman <14060565+velveteen-ant@users.noreply.github.com>
2022-01-19 00:16:13 -0500

    need to fix error loading

--- src/components/FlightModal.jsx
+++ src/components/FlightModal.jsx
@@ -5,6 +5,7 @@ import 'regenerator-runtime';
 import ReactModal from 'react-modal';
 import Loader from './Spinner';
 import { data } from 'autoprefixer';
+import { MdError } from 'react-icons/md';
 
 const FlightModal = ({ tripLocationInfo }) => {
   //component did mount needs to get airports
@@ -14,6 +15,7 @@ const FlightModal = ({ tripLocationInfo }) => {
 
   const [modalIsOpen, setModalIsOpen] = useState(true);
   const [loading, setLoading] = useState(true);
+  const [airportCodeFound, setAirportCodeFound] = useState(false);
   const [airportSelection, setAirportSelection] = useState({
     originSelection: [],
     destinationSelection: [],
@@ -69,6 +71,10 @@ const FlightModal = ({ tripLocationInfo }) => {
       const destinationRes = await axios.get(
         `/api/flights/airport/${destinationUrl}`
       );
+
+      if (originRes.data.length > 0 && destinationRes.data.length > 0) {
+        setAirportCodeFound(true);
+      }
 
       setAirportSelection({
         ...airportSelection,
@@ -158,9 +164,8 @@ const FlightModal = ({ tripLocationInfo }) => {
 
   return (
     <div>
-      {loading ? (
-        <Loader />
-      ) : (
+      {loading && <Loader />}
+      {airportCodeFound ? (
         <ReactModal
           isOpen={modalIsOpen}
           onRequestClose={closeModal}
@@ -333,6 +338,54 @@ const FlightModal = ({ tripLocationInfo }) => {
             >
               Cancel
             </button>
+          </div>
+        </ReactModal>
+      ) : (
+        <ReactModal
+          isOpen={modalIsOpen}
+          onRequestClose={closeModal}
+          ariaHideApp={false}
+        >
+          <div className='justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none'>
+            <div className='relative w-auto my-6 mx-auto max-w-3xl'>
+              <div className='border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none'>
+                <div className='flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t'>
+                  <h3 className='text-3xl font-semibold'>Error</h3>
+                  <button
+                    className='p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none'
+                    onClick={closeModal}
+                  >
+                    <span className='bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none'>
+                      x
+                    </span>
+                  </button>
+                </div>
+                <div className='relative p-6 flex-auto'>
+                  <p className='my-4 text-blueGray-500 text-lg leading-relaxed'>
+                    There was an issue finding any airports located in your
+                    selected cities. Please try again.
+                  </p>
+                </div>
+                <div className='flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b'>
+                  <div className='mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10'>
+                    <MdError
+                      className='h-6 w-6 text-red-600'
+                      aria-hidden='true'
+                    />
+                  </div>
+                  <button
+                    className='text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'
+                    type='button'
+                    onClick={() => {
+                      closeModal();
+                      setLoading(false);
+                    }}
+                  >
+                    Close
+                  </button>
+                </div>
+              </div>
+            </div>
           </div>
         </ReactModal>
       )}
