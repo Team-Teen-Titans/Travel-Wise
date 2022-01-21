80af9ae04ddb968353f071cb3362da44a0c3a9cf
jakedoublev <vanvorhisjake@gmail.com>
2022-01-20 19:09:48 -0800

    now rendering the first leg and second leg info back from server/api and a purchase link if there are flights available or text that says fully booked if not

--- src/components/FlightCard.jsx
+++ src/components/FlightCard.jsx
@@ -1,23 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
 const FlightCard = ({ flightInfo }) => {
   const {
-    legOneInfo: { airlineCodes },
+    legOneInfo,
+    legTwoInfo,
     remainingSeatsCount,
-    taxesAndFees,
     total,
-    totalPricePerPassenger,
+    handoffUrl,
+    // taxesAndFees,
+    // totalPricePerPassenger,
   } = flightInfo;
-  console.log('flightInfo',flightInfo)
+  console.log("flightInfo", flightInfo);
+
   return (
-    <div>
+    <div className="border-solid rounded-sm">
       {/* add button at the top to save a flight? */}
       <ul>
-        <li>Airline: {airlineCodes[0]}</li>
-        <li>Available Seats: {remainingSeatsCount}</li>
-        <li>Price per Passenger: ${totalPricePerPassenger}</li>
-        <li>Taxes and Fees: ${taxesAndFees}</li>
+        <li>Airline: {legOneInfo.airlineCodes[0]}</li>
+        <>
+          <h3>Outbound Trip: </h3>
+          <li>Departure Time: {legOneInfo.departureTime}</li>
+          <li>Arrival Time: {legOneInfo.arrivalTime}</li>
+          <li>Total Duration: {legOneInfo.duration}</li>
+          <li>Layovers: {legOneInfo.segments.length - 1}</li>
+        </>
+        <br />
+        {legTwoInfo && (
+          <>
+            <h3>Return Trip: </h3>
+            <li>Departure Time: {legTwoInfo.departureTime}</li>
+            <li>Arrival Time: {legTwoInfo.arrivalTime}</li>
+            <li>Total Duration: {legTwoInfo.duration}</li>
+            <li>Layovers: {legTwoInfo.segments.length - 1}</li>
+          </>
+        )}
+
+        {/* <li>Price per Passenger: ${totalPricePerPassenger}</li> */}
+        {/* <li>Taxes and Fees: ${taxesAndFees}</li> */}
         <li>Total Price: ${total}</li>
+        <br />
+        {remainingSeatsCount !== 0 ? (
+          <>
+            <li>Available Seats: {remainingSeatsCount}</li>
+            <a href={handoffUrl}>Purchase now</a>
+          </>
+        ) : (
+          <>
+            <li>Fully Booked</li>
+          </>
+        )}
       </ul>
     </div>
   );
