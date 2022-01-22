742fedece23ad26507c2c542f47d813a1be644be
jakedoublev <vanvorhisjake@gmail.com>
2022-01-22 10:03:45 -0800

    some styling added

--- src/components/CovidWorldData.jsx
+++ src/components/CovidWorldData.jsx
@@ -1,23 +1,23 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import Loader from './Spinner';
-import { globalCovidOptions } from '../utils/constants';
-import { BiWorld } from 'react-icons/bi';
-import { AiFillCloseCircle } from 'react-icons/ai';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Loader from "./Spinner";
+import { globalCovidOptions } from "../utils/constants";
+import { BiWorld } from "react-icons/bi";
+import { AiFillCloseCircle } from "react-icons/ai";
 
 const CovidWorldData = () => {
   const [loading, setLoading] = useState(true);
   const [toggle, setToggle] = useState(false);
   const [state, setState] = useState({
-    activeCases: '',
-    caseFatality: '',
-    newCases: '',
-    newDeaths: '',
-    newRecovered: '',
-    critical: '',
-    totalCases: '',
-    totalDeaths: '',
-    totalRecovered: '',
+    activeCases: "",
+    caseFatality: "",
+    newCases: "",
+    newDeaths: "",
+    newRecovered: "",
+    critical: "",
+    totalCases: "",
+    totalDeaths: "",
+    totalRecovered: "",
   });
 
   useEffect(() => {
@@ -48,9 +48,12 @@ const CovidWorldData = () => {
           totalDeaths: data[0].TotalDeaths.toLocaleString(undefined, {
             maximumFractionDigits: 2,
           }),
-          totalRecovered: Number(data[0].TotalRecovered).toLocaleString(undefined, {
-            maximumFractionDigits: 2,
-          }),
+          totalRecovered: Number(data[0].TotalRecovered).toLocaleString(
+            undefined,
+            {
+              maximumFractionDigits: 2,
+            }
+          ),
         });
       })
       .catch((err) => {
@@ -58,57 +61,69 @@ const CovidWorldData = () => {
       });
   }, []);
   return (
-    <div className='flex flex-col min-h-screen flex h-screen'>
-      <h1 align='center' className='py-4 text-3xl font-mono border-2 rounded'>
+    <div className="flex flex-col min-h-screen flex h-screen">
+      <h1 align="center" className="py-4 text-3xl font-mono border-2 rounded">
         Global Covid Data
       </h1>
       {loading ? (
-        <Loader />
+        <div className="flex justify-center items-center h-full">
+          <Loader />
+        </div>
       ) : (
         <div>
           <div>
-            <div className='my-6 mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-green-100 sm:h-20 sm:w-20'>
-              <BiWorld className='h-10 w-10 text-green-600 animate-pulse' />
+            <div className="my-6 mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-green-100 sm:h-20 sm:w-20">
+              <BiWorld className="h-10 w-10 text-green-600 animate-pulse" />
             </div>
-            <div className='m-2.5 p-2.5 flex justify-center items-center'>
-              <span className='text-red-500 background-transparent font-bold uppercase px-6 py-2 text-2xl outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'>
+            <div className="m-2.5 p-2.5 flex justify-center items-center">
+              <span className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-2xl outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150">
                 Active Cases: {state.activeCases}
               </span>
             </div>
-            <div className='m-2.5 p-2.5 flex justify-center items-center'>
-              <span className='text-gray-700 background-transparent font-bold uppercase px-6 py-2 text-2xl outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'>
+            <div className="m-2.5 p-2.5 flex justify-center items-center">
+              <span className="text-gray-700 background-transparent font-bold uppercase px-6 py-2 text-2xl outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150">
                 Total Cases: {state.totalCases}
               </span>
             </div>
-            <div className='m-2.5 p-2.5 flex justify-center items-center'>
-              <span className='text-gray-500 background-transparent font-bold uppercase px-6 py-2 text-2xl outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'>
+            <div className="m-2.5 p-2.5 flex justify-center items-center">
+              <span className="text-gray-500 background-transparent font-bold uppercase px-6 py-2 text-2xl outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150">
                 Total Deaths: {state.totalDeaths}
               </span>
             </div>
-            <div className='m-2.5 p-2.5 flex justify-center items-center'>
-              <span className='text-green-500 background-transparent font-bold uppercase px-6 py-2 text-2xl outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'>
+            <div className="m-2.5 p-2.5 flex justify-center items-center">
+              <span className="text-green-500 background-transparent font-bold uppercase px-6 py-2 text-2xl outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150">
                 Total Recovered: {state.totalRecovered}
               </span>
             </div>
           </div>
-          <div className='m-2.5 p-2.5 flex justify-center items-center'>
+          <div className="m-2.5 p-2.5 flex justify-center items-center">
             <button
               onClick={() => setToggle(true)}
-              className='border 1px rounded text-blue-400 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'
+              className="border 1px rounded text-blue-400 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
             >
               View More
             </button>
           </div>
           {toggle && (
-            <div className='m-2.5 p-2.5 flex justify-center items-center bg-gray-300 bg-opacity-30'>
-              <span className='m-2.5 p-2.5 flex justify-center items-center text-gray-900 font-bold uppercase px-6 py-2 text-l outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'>New Cases: {state.newCases}</span>
-              <span className='m-2.5 p-2.5 flex justify-center items-center text-gray-900 font-bold uppercase px-6 py-2 text-l outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'>New Deaths: {state.newDeaths}</span>
-              <span className='m-2.5 p-2.5 flex justify-center items-center text-gray-900 font-bold uppercase px-6 py-2 text-l outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'>New Recovered: {state.newRecovered}</span>
-              <span className='m-2.5 p-2.5 flex justify-center items-center text-gray-900 font-bold uppercase px-6 py-2 text-l outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'>Critical Cases: {state.critical}</span>
-              <span className='m-2.5 p-2.5 flex justify-center items-center text-gray-900 font-bold uppercase px-6 py-2 text-l outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'>Case Fatality: {state.caseFatality}%</span>
+            <div className="m-2.5 p-2.5 flex justify-center items-center bg-gray-300 bg-opacity-30">
+              <span className="m-2.5 p-2.5 flex justify-center items-center text-gray-900 font-bold uppercase px-6 py-2 text-l outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150">
+                New Cases: {state.newCases}
+              </span>
+              <span className="m-2.5 p-2.5 flex justify-center items-center text-gray-900 font-bold uppercase px-6 py-2 text-l outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150">
+                New Deaths: {state.newDeaths}
+              </span>
+              <span className="m-2.5 p-2.5 flex justify-center items-center text-gray-900 font-bold uppercase px-6 py-2 text-l outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150">
+                New Recovered: {state.newRecovered}
+              </span>
+              <span className="m-2.5 p-2.5 flex justify-center items-center text-gray-900 font-bold uppercase px-6 py-2 text-l outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150">
+                Critical Cases: {state.critical}
+              </span>
+              <span className="m-2.5 p-2.5 flex justify-center items-center text-gray-900 font-bold uppercase px-6 py-2 text-l outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150">
+                Case Fatality: {state.caseFatality}%
+              </span>
               <AiFillCloseCircle
                 onClick={() => setToggle(false)}
-                className='cursor-pointer text-red-600 rounded-full'
+                className="cursor-pointer text-red-600 rounded-full"
               />
             </div>
           )}
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import { Link, useNavigate } from 'react-router-dom';
-import { MdAccountCircle } from 'react-icons/md';
-import { FcGoogle } from 'react-icons/fc';
+import React, { useState } from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import { MdAccountCircle } from "react-icons/md";
+import { FcGoogle } from "react-icons/fc";
 
 const Login = ({ setRefetchAuth }) => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [loginError, setLoginError] = useState(false);
   const navigate = useNavigate();
 
@@ -22,11 +22,11 @@ const Login = ({ setRefetchAuth }) => {
   const handleSubmit = (e) => {
     e.preventDefault();
     axios
-      .post('/api/user/login', { email: email, password: password })
+      .post("/api/user/login", { email: email, password: password })
       .then((res) => {
         if (res.status === 200) {
-          navigate('/');
-          setRefetchAuth(prev => ++prev)
+          navigate("/");
+          setRefetchAuth((prev) => ++prev);
           // window.location.reload();
         }
         // sessionStorage.setItem('email', email);
@@ -34,43 +34,43 @@ const Login = ({ setRefetchAuth }) => {
         // window.location.href = '/home';
       })
       .catch((err) => {
-        console.log('error in axios in Login.jsx:', err);
+        console.log("error in axios in Login.jsx:", err);
         setLoginError(true);
       });
   };
 
   return (
-    <div className='flex h-screen bg-indigo-700'>
-      <div className='w-full max-w-xs m-auto bg-indigo-100 rounded p-5'>
+    <div className="flex h-screen bg-gradient-to-r from-blue-400 via-indigo-400 to-blue-400">
+      <div className="w-full max-w-xs m-auto bg-indigo-100 rounded p-5">
         <header>
-          <MdAccountCircle className='h-10 w-10 text-indigo-700 mx-auto mb-5' />
+          <MdAccountCircle className="h-10 w-10 text-indigo-700 mx-auto mb-5" />
         </header>
-        <form id='login'>
+        <form id="login">
           <div>
-            <label className='block mb-2 text-indigo-500'>
+            <label className="block mb-2 text-indigo-500">
               Email:
               <input
-                className='w-full p-2 mb-6 text-indigo-700 border-b-2 border-indigo-500 outline-none focus:bg-gray-300'
-                type='text'
+                className="w-full p-2 mb-6 text-indigo-700 border-b-2 border-indigo-500 outline-none focus:bg-gray-300"
+                type="text"
                 onChange={onEmailChange}
                 required
               />
             </label>
           </div>
           <div>
-            <label className='block mb-2 text-indigo-500'>
+            <label className="block mb-2 text-indigo-500">
               Password:
               <input
-                className='w-full p-2 mb-6 text-indigo-700 border-b-2 border-indigo-500 outline-none focus:bg-gray-300'
-                type='password'
+                className="w-full p-2 mb-6 text-indigo-700 border-b-2 border-indigo-500 outline-none focus:bg-gray-300"
+                type="password"
                 onChange={onPasswordChange}
                 required
               />
             </label>
             <div>
               <button
-                className='w-full bg-indigo-700 hover:bg-pink-700 text-white font-bold py-2 px-4 mb-6 rounded'
-                type='submit'
+                className="w-full bg-indigo-700 hover:bg-pink-700 text-white font-bold py-2 px-4 mb-6 rounded"
+                type="submit"
                 onClick={handleSubmit}
               >
                 Log In
@@ -79,24 +79,24 @@ const Login = ({ setRefetchAuth }) => {
           </div>
         </form>
         <footer>
-          <p className='text-indigo-700 text-sm float-right'>
-            Need to sign up? Click{' '}
-            <Link to={'/signup'} className='hover:text-pink-700'>
+          <p className="text-indigo-700 text-sm float-right">
+            Need to sign up? Click{" "}
+            <Link to={"/signup"} className="hover:text-pink-700">
               here
-            </Link>{' '}
+            </Link>{" "}
             to get started!
           </p>
-          <div className='space-x-1'>
-            <FcGoogle className='inline-block' />
+          <div className="space-x-1">
+            <FcGoogle className="inline-block" />
             <a
-              className='text-indigo-700 hover:text-pink-700 text-sm inline-block'
-              href='/api/user/google'
+              className="text-indigo-700 hover:text-green-300 text-sm inline-block"
+              href="/api/user/google"
             >
               Authenticate with Google
             </a>
           </div>
           {loginError && (
-            <p className='text-center w-full text-pink-900 font-bold py-2 px-4 mb-6 rounded'>
+            <p className="text-center w-full text-pink-900 font-bold py-2 px-4 mb-6 rounded">
               Invalid email or password
             </p>
           )}
