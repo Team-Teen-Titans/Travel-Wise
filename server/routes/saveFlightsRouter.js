ef88275a50e55960d0607d07ec2850e973c01ae7
TX Ho <tingxianho@gmail.com>
2022-01-23 03:33:39 +0800

    Save only if logged in, other styling changes

--- server/routes/saveFlightsRouter.js
+++ server/routes/saveFlightsRouter.js
@@ -5,17 +5,31 @@ const {
 	saveFlights,
 	deleteFlights,
 } = require('../controllers/savedFlightsController');
+const {
+	checkAuthenticated,
+	checkNotAuthenticated,
+} = require('../controllers/sessionController');
 
-router.get('/get-saved-flights', getSavedFlights, (req, res) => {
-	return res.status(200).send(res.locals.savedFlights);
-});
+router.get(
+	'/get-saved-flights',
+	checkAuthenticated,
+	getSavedFlights,
+	(req, res) => {
+		return res.status(200).send(res.locals.savedFlights);
+	}
+);
 
-router.post('/save-flights', saveFlights, (req, res) => {
+router.post('/save-flights', checkAuthenticated, saveFlights, (req, res) => {
 	return res.status(200).send('Your flight has been saved.');
 });
 
-router.delete('/delete-saved-flights', deleteFlights, (req, res) => {
-	return res.status(200).send('Your flight has been deleted.');
-});
+router.delete(
+	'/delete-saved-flights',
+	checkAuthenticated,
+	deleteFlights,
+	(req, res) => {
+		return res.status(200).send('Your flight has been deleted.');
+	}
+);
 
 module.exports = router;
